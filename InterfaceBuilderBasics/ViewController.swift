675dcb12aad97339a64ddbf2f6aad8cee9a9f1d4
Saya M <saya.murton@gmail.com>
2018-05-19 15:15:54 -0600

    Action
    -created action `changeTitle`

--- InterfaceBuilderBasics/ViewController.swift
+++ InterfaceBuilderBasics/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var mainLabel: UILabel!
+    @IBAction func changeTitle(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
